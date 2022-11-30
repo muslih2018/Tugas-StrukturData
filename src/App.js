38757d08ce44ea5497fd7b20027f050e65bbce81
MUSLIH SAHMAT <106954335+muslih2018@users.noreply.github.com>
2022-11-30 14:32:46 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/rNC-D1NyyBP6QQVg/scene.splinecode" />
+  );
+}
