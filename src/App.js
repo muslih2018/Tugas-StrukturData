3773f6716b9f7f877ca741d66c26926ecc7fa374
MUSLIH SAHMAT <106954335+muslih2018@users.noreply.github.com>
2022-12-21 14:20:29 +0700

    Delete src directory

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/rNC-D1NyyBP6QQVg/scene.splinecode" />
-  );
-}
